e00122599e76d2a1ea6759e8768ef2067a5c098c
Carla Redmond <carla.redmond@student.stenden.com>
2019-04-10 13:56:15 +0200

    added binary search tree
    CREATION ONLY

---
+++ dsa/src/BST.java
@@ -0,0 +1,49 @@
+class Node<T extends Comparable<T>> {
+    private T value;
+    private Node<T> left;
+    private Node<T> right;
+
+    public Node(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public Node<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<T> left) {
+        this.left = left;
+    }
+
+    public Node<T> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<T> right) {
+        this.right = right;
+    }
+}
+
+
+class BST <T extends Comparable<T>> {
+    private Node<T> root;
+
+    public BST(T value) {
+        root = new Node<T>(value);
+    }
+
+    public void insert(T value) {
+        Node<T> node = new Node<T>(value);
+        // insert logic goes here to search and insert
+    }
+}
+
+
