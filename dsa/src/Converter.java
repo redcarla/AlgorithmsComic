33d284926322fd70e5ce696b0ff636d5df5c2f9a
Ramonb2 <Ramon.brakels@student.stenden.com>
2019-04-10 22:52:43 +0200

    Made character object
    *zucht*

--- dsa/src/Converter.java
+++ dsa/src/Converter.java
@@ -1,15 +1,18 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Converter {
+    private static ArrayList<DcChar> charac;
+    private static LinkedList<DcChar> listy;
     public static void main(String[] args) {
 
         BufferedReader newBuffer = null;
 
         try {
             String newLine;                                         //whole line from csv
-            newBuffer = new BufferedReader(new FileReader("/Users/carlacatherine/Downloads/dsa/src/dc-wikia-data.csv"));
+            newBuffer = new BufferedReader(new FileReader("C:\\Users\\ramon\\Documents\\GitHub\\AlgorithmsComic\\AlgorithmsComic\\dsa\\src\\dc-wikia-data.csv"));
 
 
             while ((newLine = newBuffer.readLine()) != null) {
@@ -32,50 +35,75 @@ public class Converter {
 
 
     public static void Converter(String newCSV) {                   //convert csv using split
-        LinkedList llist = new LinkedList();                        //new LINKED LIST
+        listy = new LinkedList<>();                        //new LINKED LIST
         Stack stackie = new Stack();                                //new STACK
         Queue q = new Queue();                                      //new QUEUE
         DLL dll = new DLL();                                        //new DOUBLY LINKED LIST
         BinaryST bst = new BinaryST();
+        charac = new ArrayList<>();
 
 
         if (newCSV != null) {
             String[] splitData = newCSV.split("\\s*,\\s*");   //split on comma
             for (int i = 0; i < splitData.length; i++) {
                 if (!(splitData[i] == null) || !(splitData[i].length() == 0) || splitData[i].isEmpty()) {
+                    //System.out.println(splitData[i]);
+
+                    if(i ==2) {
+                        DcChar character = new DcChar();
+                        character.setName(splitData[1]);
+                        charac.add(character);
+
+                        listy.append(character);
+                    }
+
+
+                    /*//System.out.println(character.getName());
+
+                    //q.enqueue(character.getName());
                     llist.append(splitData[i].trim());              //add to linked list
                     dll.append(splitData[i].trim());                //add to doubly linked list
                     stackie.push(splitData[i].trim());              //add to stack
                     q.enqueue(splitData[i].trim());                 //add to queue
-                    bst.insert(splitData[i].trim());                   //add to binary search tree
+                    bst.insert(splitData[i].trim());    */               //add to binary search tree
                 }
             }
         }
-        System.out.println("LList: ");
-            llist.printList();
-        System.out.println();
-        System.out.println("DLL:");
-            dll.printlist(dll.head);
-
-        System.out.println("\n Stack:");
-        for (int i = 0; i < 12; i++) {                              //loop through each one to print
-            System.out.println(stackie.pop());
-
+        //System.out.println("LList: ");
+        for (DcChar chara : charac){
+            System.out.println(chara.getName());
         }
+          //  llist.printList();
+        listy.printList();
+        //System.out.println();
+        //System.out.println("DLL:");
+          //  dll.printlist(dll.head);
 
+        /*System.out.println("\n Stack:");
+        for (int i = 0; i < 12; i++) {                              //loop through each one to print
+            try {
+                System.out.println(stackie.pop());
+                var test = stackie.pop();
+                System.out.println(test.getClass());
+            } catch(NullPointerException n) {
+
+            }
+        }
+/*
 
         System.out.println("\n Queue:");
         for (int i = 0; i < 12; i++) {                              //loop through each one to print
             try {
                 System.out.println(q.dequeue().key);
+
             }
             catch (NullPointerException d) {                        // to get rid of any NPE's
 
             }
-        }
+        } /*
         System.out.println("BinaryST:");
         bst.preOrder();
-
+*/
         }
 
 
---
+++ dsa/src/DcChar.java
@@ -0,0 +1,106 @@
+public class DcChar {
+    String name;
+    int id;
+    String sex;
+    String align;
+    String eye;
+    String hair;
+    String GSM;
+    String alive;
+    String appe;
+    String fappe;
+    String year;
+
+    public DcChar(){
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getId(){
+        return id;
+    }
+
+    public void setSex(String sex){
+        this.sex = sex;
+    }
+
+    public String getSex(){
+        return sex;
+    }
+
+    public void setAlign(String align){
+        this.align = align;
+    }
+
+    public String getAlign(){
+        return align;
+    }
+
+    public void setEye(String eye){
+        this.eye = eye;
+    }
+
+    public String getEye(){
+        return eye;
+    }
+
+    public void setHair(String hair){
+        this.hair = hair;
+    }
+
+    public String getHair(){
+        return hair;
+    }
+
+    public void setGSM(String GSM){
+        this.GSM = GSM;
+    }
+
+    public String getGSM(){
+        return GSM;
+    }
+
+    public void setAlive(String alive){
+        this.alive = alive;
+    }
+
+    public String getAlive(){
+        return alive;
+    }
+
+    public void setappearance(String appe){
+        this.appe = appe;
+    }
+
+    public String getAppearance(){
+        return appe;
+    }
+
+    public void setFAppearance(String fappe){
+        this.fappe = fappe;
+    }
+
+    public String getFAppearance(){
+        return fappe;
+    }
+
+    public void setYear(String year){
+        this.year = year;
+    }
+
+    public String getYear(){
+        return year;
+    }
+
+    @Override public String toString() {
+        return "Player: " + name;
+    }
+}
